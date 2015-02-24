a64f2ecfee097d16d1f9203ad0def28fd06549a3
lenny@erwinefamily.net <lenny@erwinefamily.net>
2015-02-24 07:17:14 -0500

    Added installer files

---
+++ WinForms/Installer/Test.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module -Name:'WinForms'
+
+$options = @('one', 'two', 'three')
+$result = $options | Get-SelectionFromGUI -Title:'My FOrm' -HeadingText:'MY Heading Test';
+$result.TerminatingAction;
+$result.SelectedValues
