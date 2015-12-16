886c0c4edf886ccaaa608be82439a51b1c433c43
Leonard <lenny@erwinefamily.net>
2015-12-16 09:19:10 -0500

    CLR Version 2.0 compatibility
    Made changes so it is compatible with CLR and PowerShell versions 2.0.

--- WindowsForms/WindowsForms/Uninstall.ps1
+++ WindowsForms/WindowsForms/Uninstall.ps1
@@ -19,9 +19,10 @@ if ($ModuleInfo -eq $null) {
         ) | Write-Warning;
     }
 } else {
-    $result = $Host.UI.PromptForChoice("Confirm Uninstall", ('Are you sure you want to remove module {0} from {1}?' -f $ModuleBaseName, $ModuleInfo.ModuleBase), @(
-        (New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"Yes"),
-        (New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"No")), 1);
+    $Choices = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription]';
+    $Choices.Add((New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"Yes"));
+    $Choices.Add((New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"No"));
+    $result = $Host.UI.PromptForChoice("Confirm Uninstall", ('Are you sure you want to remove module {0} from {1}?' -f $ModuleBaseName, $ModuleInfo.ModuleBase), $Choices, 1);
     if ($result -ne $null -and $result -eq 0) {
         Remove-Module -Name:$ModuleBaseName;
         Remove-Item -Path:($ModuleInfo.ModuleBase) -Force -Recurse;
