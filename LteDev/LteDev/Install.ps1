bad451cc12c3de7f25ff361cc5e5668e2ccddca2
Leonard <lenny@erwinefamily.net>
2015-12-16 09:10:58 -0500

    CLR Version 2.0 compatibility
    Made changes so it is compatible with CLR and PowerShell versions 2.0.

--- LteDev/LteDev/Install.ps1
+++ LteDev/LteDev/Install.ps1
@@ -11,9 +11,10 @@ if ($installedAt.Count -gt 0) {
         '',
 		'Do you wish to overwrite?'
     ) | Out-String).Trim();
-    $result = $Host.UI.PromptForChoice("Confirm Overwrite", $Message, @(
-        (New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"Yes"),
-        (New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"No")), 1);
+    $Choices = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription]';
+    $Choices.Add((New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"Yes"));
+    $Choices.Add((New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:"No"));
+    $result = $Host.UI.PromptForChoice("Confirm Overwrite", $Message, $Choices, 1);
 	if ($result -eq $null -or $result -ne 0) {
 		'Aborted.' | Write-Warning;
 		return;
@@ -21,15 +22,15 @@ if ($installedAt.Count -gt 0) {
 }
 
 $i = 0;
-$choices = @(
-    $env:PSModulePath.Split([System.IO.Path]::PathSeparator) | ForEach-Object {
-        $i = $i + 1;
-        (New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:($i, $_))
-    }
-);
+$Choices = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription]';
+$env:PSModulePath.Split([System.IO.Path]::PathSeparator) | ForEach-Object {
+    $i = $i + 1;
+    $Choices.Add((New-Object -TypeName:'System.Management.Automation.Host.ChoiceDescription' -ArgumentList:($i, $_)));
+}
 
-$result = $Host.UI.PromptForChoice("Select Module Location", (@($choices | ForEach-Object { '{0}: {1}' -f $_.Label, $_.HelpMessage }) | Out-String), $choices, 1);
-$ModuleInstallLocation = $choices[$result].HelpMessage | Join-Path -ChildPath:$ModuleBaseName;
+
+$result = $Host.UI.PromptForChoice("Select Module Location", (@($Choices | ForEach-Object { '{0}: {1}' -f $_.Label, $_.HelpMessage }) | Out-String).Trim(), $Choices, 0);
+$ModuleInstallLocation = $Choices[$result].HelpMessage | Join-Path -ChildPath:$ModuleBaseName;
 
 if (-not ($ModuleInstallLocation | Test-Path)) {
     $folder = New-Item -Path:$ModuleInstallLocation -ItemType:'Directory';
@@ -40,6 +41,7 @@ if (-not ($ModuleInstallLocation | Test-Path)) {
     }
 }
 
+if ($PSScriptRoot -eq $null) { $PSScriptRoot = Get-Location }
 $fileName = $ModuleBaseName + '.psm1';
 $source = $PSScriptRoot | Join-Path -ChildPath:$fileName;
 $destination = $ModuleInstallLocation | Join-Path -ChildPath:$fileName;
