13f84cb21da1251502a1ee5ac6883b12428bc972
Leonard <lenny@erwinefamily.net>
2016-04-01 16:08:00 -0400

    Added web request initialization functions

--- Erwine.Leonard.T.NetworkUtility/Erwine.Leonard.T.NetworkUtility.psm1
+++ Erwine.Leonard.T.NetworkUtility/Erwine.Leonard.T.NetworkUtility.psm1
@@ -64,6 +64,237 @@ Function New-WebRequest {
     }
 }
 
+Function Initialze-WebRequestPostForm {
+    [CmdletBinding(DefaultParameterSetName = 'GetRequest')]
+    [OutputType([System.Net.WebRequest], ParameterSetName = 'GetRequest')]
+    [OutputType([System.Management.Automation.PSObject], ParameterSetName = 'GetResponse')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [System.Net.WebRequest]$WebRequest,
+        
+        [Parameter(Mandatory = $true)]
+        [Hashtable]$FormData,
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'GetRequest')]
+        [switch]$PassThru,
+        
+        [Parameter(ParameterSetName = 'GetResponse')]
+        [bool]$AllowRedirect,
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'GetResponse')]
+        [switch]$GetResponse
+    )
+    
+    Process {
+        $WebRequest.Method = 'POST';
+        
+        $WebRequest.ContentType = 'application/x-www-form-urlencoded';
+        $Stream = $WebRequest.GetRequestStream();
+        try {
+            $StreamWriter = New-Object -TypeName 'System.IO.StreamWriter' -ArgumentList $Stream ([System.Text.Encoding]::ASCII);
+            try {
+                $keyWritten = $false;
+                foreach ($key in $Formdata.Keys) {
+                    if ($keyWritten) { $StreamWriter.Write('&') } else { $keyWritten = $true }
+                    $StreamWriter.Write([System.Uri]::EscapeDataString($key));
+                    $StreamWriter.Write('=');
+                    if ($FormData[$key] -ne $null) {
+                        if ($FormData[$key] -is [string]) {
+                            $StreamWriter.Write([System.Uri]::EscapeDataString($Formdata[$key]));
+                        } else {
+                            $StreamWriter.Write([System.Uri]::EscapeDataString((($Formdata[$key] | Out-String) -replace '(\r\n?|\n)$', ''));
+                        }
+                    }
+                }
+            } catch {
+                throw;
+            } finally {
+                $StreamWriter.Flush();
+                $Stream.Flush();
+                $WebRequest.ContentLength = [int]$Stream.Position;
+                $StreamWriter.Close();
+                $StreamWriter.Dispose();
+                $Stream = $null;
+            }
+        } catch {
+            throw;
+        } finally {
+            if ($Stream -ne $null) { $Stream.Dispose() }
+        }
+        if ($GetResponse) {
+            if ($PSBoundParameters.ContainsKey('AllowRedirect')) { $WebRequest.AllowRedirect = $AllowRedirect }
+            $WebRequest.GetResponse() | Write-Output;
+        } else {
+            if ($PassThru) { $WebRequest | Write-Output };
+        }
+    }
+}
+
+Function Initialize-WebRequestPostXml {
+    [CmdletBinding(DefaultParameterSetName = 'GetRequest')]
+    [OutputType([System.Net.WebRequest], ParameterSetName = 'GetRequest')]
+    [OutputType([System.Management.Automation.PSObject], ParameterSetName = 'GetResponse')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [System.Net.WebRequest]$WebRequest,
+        
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlNode]$XmlData,
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'GetRequest')]
+        [switch]$PassThru,
+        
+        [Parameter(ParameterSetName = 'GetResponse')]
+        [bool]$AllowRedirect,
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'GetResponse')]
+        [switch]$GetResponse
+    )
+    
+    Process {
+        $WebRequest.Method = 'POST';
+        
+        $WebRequest.ContentType = 'text/xml;charset=utf-8';
+        $Stream = $WebRequest.GetRequestStream();
+        try {
+            $XmlWriterSettings = New-Object -TypeName 'System.Xml.XmlWriterSettings' -Property @{
+                Indent = $true;
+                CloseOutput = $true;
+                Encoding = [System.Text.Encoding]::UTF8;
+                OmitXmlDeclaration = $true;
+            };
+            $XmlWriter = [System.Xml.XmlWriter]::Create($Stream, $XmlWriterSettings);
+            try {
+                $XmlData.WriteTo($XmlWriter);
+            } catch {
+                throw;
+            } finally {
+                $XmlWriter.Flush();
+                $Stream.Flush();
+                $WebRequest.ContentLength = [int]$Stream.Position;
+                $XmlWriter.Close();
+                $XmlWriter = $null;
+                $Stream = $null;
+            }
+        } catch {
+            throw;
+        } finally {
+            if ($Stream -ne $null) { $Stream.Dispose() }
+        }
+        if ($GetResponse) {
+            if ($PSBoundParameters.ContainsKey('AllowRedirect')) { $WebRequest.AllowRedirect = $AllowRedirect }
+            $WebRequest.GetResponse() | Write-Output;
+        } else {
+            if ($PassThru) { $WebRequest | Write-Output };
+        }
+    }
+}
+
+Function Get-WebResponse {
+    [CmdletBinding()]
+    [OutputType([System.Management.Automation.PSObject])]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [System.Net.WebRequest]$WebRequest,
+        
+        [bool]$AllowRedirect
+    )
+    
+    Process {
+        if ($PSBoundParameters.ContainsKey('AllowRedirect')) { $WebRequest.AllowRedirect = $AllowRedirect }
+        $Response = @{
+            Request = $WebRequest;
+            ErrorStatus = [System.Net.WebExceptionStatus]::UnknownError;
+            StatusCode = [System.Management.Automation.PSInvocationState]::NotStarted;
+            Success = $false;
+            StatusDescription = 'Request not sent.';
+        };
+        try {
+            $Response['Response'] = $WebRequest.GetResponse();
+            $Response['ErrorStatus'] = [System.Net.WebExceptionStatus]::Success;
+            if ($Response['Response'].StatusCode -ne $null) {
+                $Response['StatusCode'] = $Response['Response'].StatusCode;
+                $Response['Success'] = $SuccessCodes -contains $Response['StatusCode'];
+            } else {
+                $Response['StatusCode'] = [System.Management.Automation.PSInvocationState]::Completed;
+                $Response['Success'] = $true;
+            }
+            if ($Response['Response'].StatusDescription -eq $null -or $Response['Response'].StatusDescription.Trim() -eq '') {
+                $Response['StatusDescription'] = $Response['StatusCode'].ToString('F');
+            } else {
+                $Response['StatusDescription'] = $Response['Response'].StatusDescription;
+            }
+            
+        } catch [System.Net.WebException] {
+            $Response['ErrorStatus'] = $_.Exception.Status;
+            $Response['Response'] = $_.Exception.Response;
+            $Response['Error'] = $_;
+            if ($Response['Response'] -ne $null -and $Response['Response'].StatusCode -ne $null) {
+                $Response['StatusCode'] = $Response['Response'].StatusCode;
+            } else {
+                $Response['StatusCode'] = [System.Management.Automation.PSInvocationState]::Failed;
+            }
+            if ($Response['Response'] -eq $null -or $Response['Response'].StatusDescription -eq $null -or $Response['Response'].StatusDescription.Trim() -eq '') {
+                if ($_.ErrorDetails -eq $null -or $_.ErrorDetails.Message -eq $null -or $_.ErrorDetails.Message.Trim() -eq '') {
+                    if ($_.Exception -eq $null -or $_.Exception.Message -eq $null -or $_.Exception.Message.Trim() -eq '') {
+                        $Response['StatusDescription'] = $Response['StatusCode'].ToString('F');
+                    } else {
+                        $Response['StatusDescription'] = $_.Exception.Message;
+                    }
+                } else {
+                    $Response['StatusDescription'] = $_.ErrorDetails.Message;
+                }
+            } else {
+                $Response['StatusDescription'] = $Response['Response'].StatusDescription;
+            }
+        } catch [System.Net.Sockets.SocketException] {
+            $Response['StatusCode'] = $_.SocketErrorCode;
+            $Response['Error'] = $_;
+            if ($_.ErrorDetails -eq $null -or $_.ErrorDetails.Message -eq $null -or $_.ErrorDetails.Message.Trim() -eq '') {
+                if ($_.Exception -eq $null -or $_.Exception.Message -eq $null -or $_.Exception.Message.Trim() -eq '') {
+                    $Response['StatusDescription'] = $Response['StatusCode'].ToString('F');
+                } else {
+                    $Response['StatusDescription'] = $_.Exception.Message;
+                }
+            } else {
+                $Response['StatusDescription'] = $_.ErrorDetails.Message;
+            }
+        } catch {
+            $Response['StatusCode'] = [System.Management.Automation.PSInvocationState]::Failed;
+            if ($_.Exception -ne $null -and $_.Exception -is [System.Management.Automation.MethodException]) {
+                if ($_.Exception.ErrorRecord -ne $null) {
+                    $Response['Error'] = $_.Exception.ErrorRecord;
+                } else {
+                    $Response['Error'] = $_;
+                }
+                if ($_.Exception.InnerException -ne $null) {
+                    $e = $_.Exception.InnerException;
+                } else {
+                    $e = $null;
+                }
+            } else {
+                $e = $null;
+            }
+            if ($e -ne $null -and $e.Message -ne $null -and $e.Message.Trim() -ne '') {
+                $Response['StatusDescription'] = $e.Message;
+            } else {
+                if ($Response['Error'].ErrorDetails -eq $null -or $Response['Error'].ErrorDetails.Message -eq $null -or $Response['Error'].ErrorDetails.Message.Trim() -eq '') {
+                    if ($Response['Error'].Exception -eq $null -or $Response['Error'].Exception.Message -eq $null -or $Response['Error'].Exception.Message.Trim() -eq '') {
+                        $Response['StatusDescription'] = $Response['StatusCode'].ToString('F');
+                    } else {
+                        $Response['StatusDescription'] = $Response['Error'].Exception.Message;
+                    }
+                } else {
+                    $Response['StatusDescription'] = $Response['Error'].ErrorDetails.Message;
+                }
+            }
+        }
+        New-Object -TypeName 'System.Management.Automation.PSObject' -Parameter $Response;
+        if ($PSBoundParameters.ContainsKey('AllowRedirect')) { $WebRequest.AllowRedirect = $AllowRedirect }
+        $WebRequest.GetResponse() | Write-Output;
+    }
+}
+
 Function Test-SoapEnvelope {
     [CmdletBinding(DefaultParameterSetName = 'XmlNamespaceManager')]
     [OutputType([bool])]
