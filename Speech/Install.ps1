7288733aca33880d1793636e019bbdeeb9fe6165
Leonard <lenny@erwinefamily.net>
2017-04-12 13:31:03 -0400

    Merged new code

--- Speech/Install.ps1
+++ Speech/Install.ps1
@@ -21,17 +21,17 @@ Function Get-SpecialFolderPath {
     }
 }
 
-$AllUsersBasePaths = @(([System.Environment+SpecialFolder]::CommonApplicationData,
+$AllUsersBasePaths = @(([System.Environment+SpecialFolder]::ProgramFiles,
+    [System.Environment+SpecialFolder]::ProgramFilesX86,
+    [System.Environment+SpecialFolder]::CommonApplicationData,
     [System.Environment+SpecialFolder]::CommonDesktopDirectory,
     [System.Environment+SpecialFolder]::CommonProgramFiles,
     [System.Environment+SpecialFolder]::CommonProgramFilesX86,
     [System.Environment+SpecialFolder]::CommonPrograms,
-    [System.Environment+SpecialFolder]::ProgramFiles,
-    [System.Environment+SpecialFolder]::ProgramFilesX86,
     [System.Environment+SpecialFolder]::Programs) | Get-SpecialFolderPath | Select-Object -Unique);
-$CurrentUserBasePaths = @(([System.Environment+SpecialFolder]::ApplicationData,
+$CurrentUserBasePaths = @(([System.Environment+SpecialFolder]::MyDocuments,
+    [System.Environment+SpecialFolder]::ApplicationData,
     [System.Environment+SpecialFolder]::LocalApplicationData,
-    [System.Environment+SpecialFolder]::MyDocuments,
     [System.Environment+SpecialFolder]::Personal) | Get-SpecialFolderPath | Select-Object -Unique);
 
 $Script:AvailableModules = @(Get-Module -ListAvailable | ForEach-Object {
@@ -64,14 +64,14 @@ $Script:TargetLocations = @($env:PSModulePath.Split([System.IO.Path]::PathSepara
 });
 if (($Script:TargetLocations | Where-Object { $_['Level'] -eq 2 }) -eq $null) {
     $Script:TargetLocations = $Script:TargetLocations + @(@{
-        Directory = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::ProgramFiles) | Join-Path -ChildPath 'WindowsPowerShell\Modules');
+        Directory = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList ($CurrentUserBasePaths[0] | Join-Path -ChildPath 'WindowsPowerShell\Modules');
         IsInModulePath = $false;
         Level = 2;
     });
 }
 if (($Script:TargetLocations | Where-Object { $_['Level'] -eq 1 }) -eq $null) {
     $Script:TargetLocations = $Script:TargetLocations + @(@{
-        Directory = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Personal) | Join-Path -ChildPath 'WindowsPowerShell\Modules');
+        Directory = New-Object -TypeName 'System.IO.DirectoryInfo' -ArgumentList ($AllUsersBasePaths[0] | Join-Path -ChildPath 'WindowsPowerShell\Modules');
         IsInModulePath = $false;
         Level = 1;
     });
@@ -93,63 +93,5 @@ $Script:TargetLocations | ForEach-Object {
     }
 }
 
-
-
-Function Get-SpecialFolderPaths
-{
-  Param(
-    [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-    [System.Environment+SpecialFolder]$SpecialFolder
-  )
-  Process {
-    $Path = [System.Environment+SpecialFolder]::GetFolderPath($_);
-    if (-not [System.String]::IsNullOrEmpty($Path)) { [System.IO.Path]::GetFullPath($Path) }
-  }
-}
-
-$AllUsersFolders = @([System.Environment+SpecialFolder]::CommonAdminTools, [System.Environment+SpecialFolder]::CommonApplicationData,
-  [System.Environment+SpecialFolder]::CommonProgramFiles, [System.Environment+SpecialFolder]::CommonProgramFilesX86,
-  [System.Environment+SpecialFolder]::CommonPrograms, [System.Environment+SpecialFolder]::ProgramFiles,
-  [System.Environment+SpecialFolder]::ProgramFilesX86, [System.Environment+SpecialFolder]::Programs) | Get-SpecialFolderPaths);
-$CurrentUserFolders = @([System.Environment+SpecialFolder]::MyDocuments, [System.Environment+SpecialFolder]::Personal,
-  [System.Environment+SpecialFolder]::UserProfile, [System.Environment+SpecialFolder]::LocalApplicationData,
-  [System.Environment+SpecialFolder]::ApplicationData) | Get-SpecialFolderPaths);
-  
-$InstallLocations = $PSModulePath.Split([System.IO.Path]::DirectorySeparatorChar) | ForEach-Object {
-  $Path =  [System.IO.Path]::GetFullPath($_);
-  if (($CurrentUserFolders | Where-Object {
-    $Path.Length -ge $_.Length -and [System.String]::Equals($_, $Path.Substring(0, $_.Length), [System.StringCompare]::InvariantCultureIgnoreCase)
-  }) -ne $null) {
-    New-Object -TypeName 'System.Management.Automation.PSObject' -Property @{
-      Path = $Path;
-      Type = 'Current User';
-    };
-  } else {
-    if (($AllUsersFolders | Where-Object {
-      $Path.Length -ge $_.Length -and [System.String]::Equals($_, $Path.Substring(0, $_.Length), [System.StringCompare]::InvariantCultureIgnoreCase)
-    }) -ne $null) {
-      New-Object -TypeName 'System.Management.Automation.PSObject' -Property @{
-        Path = $Path;
-        Type = 'All Users';
-      };
-    } else {
-      New-Object -TypeName 'System.Management.Automation.PSObject' -Property @{
-        Path = $Path;
-        Type = 'Admin Users';
-      };
-    }
-  }
-};
-if (@($InstallLocations | Where-Object { $_.Type -eq 'Admin Users' }).Count -eq 0)) {
-  # TODO: Add Admin Users
-  $PSHOME
-}
-if (@($InstallLocations | Where-Object { $_.Type -eq 'Current User' }).Count -eq 0)) {
-  # TODO: Add Current User
-  $HOME
-}
-if (@($InstallLocations | Where-Object { $_.Type -eq 'All Users' }).Count -eq 0)) {
-  # TODO: Add All Users
-  $AllUsersFolders[0]
-}
+$Choices = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription]';
 $Choices = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription]' -ArgumentList $Label, $Message;
