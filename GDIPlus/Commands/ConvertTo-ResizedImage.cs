260f51464b1903f0589e4f5816db43bae90e3359
lerwine <lenny@erwinefamily.net>
2018-01-03 02:00:52 -0500

    Implemented image resize command

--- GDIPlus/Commands/ConvertTo-ResizedImage.cs
+++ GDIPlus/Commands/ConvertTo-ResizedImage.cs
@@ -30,6 +30,15 @@ namespace Erwine.Leonard.T.GDIPlus.Commands
         [Parameter()]
         public PixelFormat Format { get; set; }
 
+        [Parameter()]
+        public Color BackgroundColor { get; set; }
+
+        [Parameter()]
+        public Brush Background{ get; set; }
+
+        [Parameter()]
+        public SwitchParameter KeepAspectRatio { get; set; }
+
         protected override void ProcessRecord()
         {
             foreach (Image image in Source)
@@ -47,12 +56,36 @@ namespace Erwine.Leonard.T.GDIPlus.Commands
                     Bitmap target = new Bitmap(Width, Height, (MyInvocation.BoundParameters.ContainsKey("Format")) ? Format : image.PixelFormat);
                     using (Graphics g = Graphics.FromImage(target))
                     {
-                            
+                        if (MyInvocation.BoundParameters.ContainsKey("BackgroundColor"))
+                            g.FillRectangle(new SolidBrush(BackgroundColor), new Rectangle(0, 0, Width, Height));
+                        else if (MyInvocation.BoundParameters.ContainsKey("Background"))
+                            g.FillRectangle(Background, new Rectangle(0, 0, Width, Height));
+                        if (KeepAspectRatio.IsPresent)
+                        {
+                            float sw = Convert.ToSingle(Width);
+                            float sh = Convert.ToSingle(Height);
+                            float tw = Convert.ToSingle(target.Width);
+                            float th = Convert.ToSingle(target.Height);
+                            float rh = th / sh;
+                            float rw = tw / sw;
+                            RectangleF dest;
+                            if (rw < rh)
+                                dest = new RectangleF(0.0f, (th - sh) / 2.0f, sw * rw, sh * rw);
+                            else
+                                dest = new RectangleF((tw - sw) / 2.0f, 0.0f, sw * rh, sh * rh);
+                            g.DrawImage(image, dest, new RectangleF(0.0f, 0.0f, Convert.ToSingle(image.Width), 
+                                Convert.ToSingle(image.Height)), GraphicsUnit.Pixel);
+                        }
+                        else
+                            g.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                                new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+                        g.Flush();
                     }
+                    WriteObject(image);
                 }
                 catch (Exception e)
                 {
-                    WriteError(new ErrorRecord(e, "New_Image.FromImage", ErrorCategory.InvalidData, Original));
+                    WriteError(new ErrorRecord(e, "New_Image.FromImage", ErrorCategory.InvalidData, image));
                 }
             }
         }
