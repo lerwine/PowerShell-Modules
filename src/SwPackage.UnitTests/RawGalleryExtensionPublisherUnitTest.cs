f73fb6ed07180f63f97cb860568095501c6bbd95
Leonard <lenny@erwinefamily.net>
2024-11-19 00:48:15 -0500

    Fixed test data source

--- src/SwPackage.UnitTests/RawGalleryExtensionPublisherUnitTest.cs
+++ src/SwPackage.UnitTests/RawGalleryExtensionPublisherUnitTest.cs
@@ -14,24 +14,16 @@ public class RawGalleryExtensionPublisherUnitTest
     [Test]
     public void TryCreateTest()
     {
-        var actual = RawGalleryExtensionPublisher.TryCreate(JsonNode.Parse("""
-{
-                        "publisherId": "eed56242-9699-4317-8bc7-e9f4b9bdd3ff",
-                        "publisherName": "redhat",
-                        "displayName": "Red Hat",
-                        "flags": "verified",
-                        "domain": "https://redhat.com",
-                        "isDomainVerified": true
-                    }
-""")!.AsObject(), out RawGalleryExtensionPublisher? publisher);
+        RawGalleryExtensionPublisher expected = TestData.GetFindRedhatXmlPublisher(out JsonObject jsonObject);
+        bool actual = RawGalleryExtensionPublisher.TryCreate(jsonObject, out RawGalleryExtensionPublisher? publisher);
         Assert.Multiple(() =>
         {
             Assert.That(actual, Is.True);
             Assert.That(publisher, Is.Not.Null);
-            Assert.That(publisher!.PublisherId, Is.EqualTo("eed56242-9699-4317-8bc7-e9f4b9bdd3ff"));
-            Assert.That(publisher!.PublisherName, Is.EqualTo("redhat"));
-            Assert.That(publisher!.DisplayName, Is.EqualTo("Red Hat"));
-            Assert.That(publisher!.IsDomainVerified, Is.True);
+            Assert.That(publisher!.PublisherId, Is.EqualTo(expected.PublisherId));
+            Assert.That(publisher!.PublisherName, Is.EqualTo(expected.PublisherName));
+            Assert.That(publisher!.DisplayName, Is.EqualTo(expected.DisplayName));
+            Assert.That(publisher!.IsDomainVerified, Is.EqualTo(expected.IsDomainVerified));
         });
     }
 }
